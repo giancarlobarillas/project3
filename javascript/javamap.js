8052aafe9d7adb062e5539f3938ed73aa7f59ce8
giancarlobarillas <gbarilla@hawk.iit.edu>
2018-04-18 22:46:42 -0500

    have initial location in mayaguez for map

---
+++ javascript/javamap.js
@@ -0,0 +1,7 @@
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 18.2013, lng: -67.1452},
+        zoom: 10
+    });
+          }
