d6d2d56431c950e9cbf765c6822465a5e3f943bd
giancarlobarillas <gbarilla@hawk.iit.edu>
2018-04-18 23:02:40 -0500

    add buttons and edit javascript file

--- javascript/javamap.js
+++ javascript/javamap.js
@@ -4,4 +4,13 @@ function initMap() {
         center: {lat: 18.2013, lng: -67.1452},
         zoom: 10
     });
+    google.maps.event.addDomListener(document.getElementById('1'), 'click', function () {
+
+    map.setCenter(new google.maps.LatLng(10.23,123.45));
+});
           }
+
+
+
+
+
